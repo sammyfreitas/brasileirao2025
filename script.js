aade0a60e55368d93f54312ff2b2cbcafa557ed5
Anthony Samuel Freitas <84025181+sammyfreitas@users.noreply.github.com>
2025-09-15 20:14:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("atualizarBtn").addEventListener("click", () => {
+    alert("Líder atualizado: FLAMENGO 🏆");
+  });
+
+// Apenas debug e futura expansão
+//document.addEventListener("DOMContentLoaded", () => {
+//    console.log("Página do Brasileirão 2025 carregada!");
+//  });
+
